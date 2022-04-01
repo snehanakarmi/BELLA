b72ac6d9df86ce6c38ebfd2e64586918e082cbbb
Sneha Nakarmi <71618803+snehanakarmi@users.noreply.github.com>
2022-04-01 23:38:47 +0545

    uploaded

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+import $ from jquery;
+
+
+$(window).scroll(function() {    
+    console.log("hello world");
+    // var scroll = $(window).scrollTop();
+
+    // if (scroll >= 500) {
+    //     $(".clearHeader").addClass("darkHeader");
+    // } else {
+    //     $(".clearHeader").removeClass("darkHeader");
+    // }
+});
+
